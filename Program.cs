822cdbffd929b1bdf6cf478815349514e26b6c03
Alma Engman <almaengman11@gmail.com>
2025-10-30 12:38:27 +0100

    Rensat i programcs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDistributedMemoryCache(); // Adds a default in-memory implementation of IDistributedCache
-
+builder.Services.AddDistributedMemoryCache(); 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
-    options.Cookie.HttpOnly = true; // Set the cookie to be HttpOnly
-    options.Cookie.IsEssential = true; // Make the session cookie essential
+    options.IdleTimeout = TimeSpan.FromMinutes(30); 
+    options.Cookie.HttpOnly = true; 
+    options.Cookie.IsEssential = true; 
 });
 
-builder.Services.AddHttpContextAccessor(); // Register IHttpContextAccessor
+builder.Services.AddHttpContextAccessor(); 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  
     app.UseHsts();
 }
 
